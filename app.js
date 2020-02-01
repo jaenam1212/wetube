1f14fd3a7af239b85a2aec3b19d6d86ece815b11
이재남 <jaenam@ijaenam-ui-MacBookPro.local>
2020-02-02 00:18:31 +0900

    testFile 커밋!!

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routers from "./routers";
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"))
+
+app.use(routers.home, globalRouter);
+app.use(routers.users, userRouter);
+app.use(routers.videos, videoRouter);
+
+export default app;
