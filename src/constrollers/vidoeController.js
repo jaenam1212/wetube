3ec2847dce23e411209e428e183dd534940e96b1
padoJaenam <97040622+padoJaenam@users.noreply.github.com>
2022-06-14 12:08:43 +0900

    6/14

--- src/constrollers/vidoeController.js
+++ src/constrollers/vidoeController.js
@@ -1,6 +1,42 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("See Video");
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        }
+    ];
+    res.render("home", {pageTitle: `Home`, videos});
+};
+export const see = (req, res) => {
+    
+    return res.render("watch");
+};
+export const edit = (req, res) => {
+    return res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("DeleteVideo");
+export const deleteVideo = (req, res) => {
+   
+    return res.send(`DeleteVideo #${req.params.id}`);
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { remove, remove, logout, see } from "../constrollers/userController";
+import { remove, logout, see } from "../constrollers/userController";
 
 const userRouter = express.Router();
 
